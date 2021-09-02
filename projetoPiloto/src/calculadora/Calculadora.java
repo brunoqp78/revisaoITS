d4388ccfb31081509062bdec5e03a655e05d3665
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-09-02 20:45:26 -0300

    backup do projeto Piloto

---
+++ projetoPiloto/src/calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+package calculadora;
+
+public class Calculadora {
+// atributos
+
+// métodos construtores
+	
+// métodos
+	
+	//modificador de acesso; pode haver static; tipo de retorno; nomeMetodo; parametros
+	public double somar(double valor1, double valor2) {
+		return 0;
+	}
+	
+	public double subtrair(double valor1, double valor2) {
+		return 0;
+	}
+	
+	public double dividir(double valor1, double valor2) {
+		return 0;
+	}
+	
+	public double multiplicar(double valor1, double valor2) {
+		return 0;
+	}
+	
+}
---
+++ projetoPiloto/src/calculadora/TesteManual.java
@@ -0,0 +1,11 @@
+package calculadora;
+
+public class TesteManual {
+
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		double r = calc.somar(10, 10);
+
+	}
+
+}
