87ca046b90573c048afcb2b04d882afcd2d19690
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-09-08 10:00:40 -0300

    atualizando o projeto

--- projetoPiloto/src/calculadora/TesteManual.java
+++ projetoPiloto/src/calculadora/TesteManual.java
@@ -2,10 +2,8 @@ package calculadora;
 
 public class TesteManual {
 
-	public static void main(String[] args) {
-		Calculadora calc = new Calculadora();
-		double r = calc.somar(10, 10);
-
+	public static void main (String args[]){
+	    
 	}
 
 }
